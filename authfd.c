2fd923d260fb2c92ba2509c921fb2d4895d7418a
Tim Adye <Tim.Adye@cern.ch>
2023-05-20 13:07:08 +0200

    fix test on ssh_request_reply success

--- authfd.c
+++ authfd.c
@@ -777,7 +777,7 @@ ssh_set_variable(int sock, const char *var, u_int lvar, const char *val, u_int l
 	buffer_put_char(&msg, SSH_AGENTC_SET_VARIABLE);
   buffer_put_string(&msg, var, lvar);
   buffer_put_string(&msg, val, lval);
-	if (ssh_request_reply(sock, &msg, &msg) == 0) {
+	if (ssh_request_reply(sock, &msg, &msg) != 0) {
 		buffer_free(&msg);
 		return 0;
 	}
@@ -800,7 +800,7 @@ ssh_get_variable(int sock, const char *var, u_int lvar, char **valp, u_int *lval
 	buffer_init(&msg);
 	buffer_put_char(&msg, SSH_AGENTC_GET_VARIABLE);
   buffer_put_string(&msg, var, lvar);
-	if (ssh_request_reply(sock, &msg, &msg) == 0) {
+	if (ssh_request_reply(sock, &msg, &msg) != 0) {
 		buffer_free(&msg);
 		return 0;
 	}
@@ -840,7 +840,7 @@ ssh_get_num_variables(int sock, const char *prefix, u_int lprefix, char full, Bu
   buffer_put_string(&request, prefix, lprefix);
 
 	buffer_init(identities);
-	if (ssh_request_reply(sock, &request, identities) == 0) {
+	if (ssh_request_reply(sock, &request, identities) != 0) {
 		buffer_free(&request);
 		return 0;
 	}
@@ -909,7 +909,7 @@ ssh_delete_variable(int sock, const char *var, u_int lvar, char all)
 	buffer_init(&msg);
 	buffer_put_char(&msg, all ? SSH_AGENTC_REMOVE_ALL_VARIABLES : SSH_AGENTC_REMOVE_VARIABLE);
   buffer_put_string(&msg, var, lvar);
-	if (ssh_request_reply(sock, &msg, &msg) == 0) {
+	if (ssh_request_reply(sock, &msg, &msg) != 0) {
 		buffer_free(&msg);
 		return 0;
 	}
