e1182547206f55b116ec5e2f1cdabfb8ef4ba9a4
Tim Adye <Tim.Adye@cern.ch>
2025-12-30 03:38:51 +0000

    remove .gitignore from ssh-store7-cygwin.patch. It breaks cygport.

--- ssh-store.make_patch.sh
+++ ssh-store.make_patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ $# -gt 0 ] || set -- V_7_4_P1
 cat ssh-store.BUILD
-for f in $(git diff --name-only "$1" | grep -ve '\.patch$' -e '^ssh-store\....*$' | sort); do
+for f in $(git diff --name-only "$1" | grep -ve '\.patch$' -e '^ssh-store\....*$' -e '^.gitignore' | sort); do
   git diff "$1" -- "$f"
 done
